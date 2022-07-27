04de4ddeb58f5f24148f8e6cd22a9e57f3ce4985
EJ <ej.roux13@gmail.com>
2022-07-27 07:01:09 -0700

    initializing clarifai client

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// import dependencies
+// Import dependencies
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+// This will be used by every Clarifai endpoint call.
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key 1383dc32a41b45d09a7c25e221adece7");
