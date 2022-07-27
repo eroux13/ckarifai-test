933b8a718af6e647effa87015ddb3d67fd08f121
EJ <ej.roux13@gmail.com>
2022-07-27 09:40:38 -0700

    debugged model id

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const USER_ID = 'eroux13';
 const PAT = '1383dc32a41b45d09a7c25e221adece7';
 const APP_ID = 'clarifaiTest';
 // Change these to whatever model and image URL you want to use
-const MODEL_ID = 'generalImageRecognition';
+const MODEL_ID = 'general-image-recognition';
 const IMAGE_URL = 'https://media.timeout.com/images/105862967/750/562/image.jpg';
 // This is optional.You can specify a model version or the empty string for the default
 const MODEL_VERSION_ID = '';
@@ -18,3 +18,37 @@ const stub = ClarifaiStub.grpc();
 const metadata = new grpc.Metadata();
 metadata.set("authorization", "Key " + PAT);
 
+// console.log(metadata);
+
+stub.PostModelOutputs(
+    {
+        user_app_id: {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        model_id: MODEL_ID,
+        version_id: MODEL_VERSION_ID, // This is optional. Defaults to the latest model version.
+        inputs: [
+            { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } }
+        ]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            throw new Error(err);
+        }
+
+        if (response.status.code !== 10000) {
+            throw new Error("Post model outputs failed, status: " + response.status.description);
+        }
+
+        // Since we have one input, one output will exist here.
+        const output = response.outputs[0];
+
+        console.log("Predicted concepts:");
+        for (const concept of output.data.concepts) {
+            console.log(concept.name + " " + concept.value);
+        }
+    }
+
+);
